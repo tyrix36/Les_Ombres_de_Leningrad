54e654125d52fedd4d36569c1680ac24f01330c8
tyrix36 <tyrix36+github@gmail.com>
2021-02-06 09:51:01 +0100

    commit 09h50

--- github_com_tyrix36_Les_Ombres_de_Leningrad.sh
+++ github_com_tyrix36_Les_Ombres_de_Leningrad.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 cd "D:\Dropbox\01_tout\www_inetpub\UwAmp\www\home\amp\amp-coc-story"
 
-echo "# Les_Ombres_de_Leningrad" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
+#echo "# Les_Ombres_de_Leningrad" >> README.md
+#git init
+git add .
+
+#git config --global user.email "tyrix36+github@gmail.com"
+#git config --global user.name "tyrix36"
+
+#git commit -m "first commit"
+
+
+git commit -m "commit 09h50"
+
 git branch -M main
 git remote add origin https://github.com/tyrix36/Les_Ombres_de_Leningrad.git
 git push -u origin main
